2b56c0433de833d73d63b683072ca20a5bc9f4f6
이동학 <donghak@idonghag-ui-MacBookPro.local>
2023-07-09 23:28:04 +0900

    2579번계단 오르기

--- donghakl/7_week-DP/계단_오르기.cpp
+++ donghakl/7_week-DP/계단_오르기.cpp
@@ -8,15 +8,19 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n, value;
+	int n = 4, value;
 	cin >> n;
 	int *arr = new int[n + 1];
-	vector <pair <int, int > > arr[n + 1];
+	int *dp = new int[n + 1];
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
 
-	for (int i = 0; i <= n; i++)
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+	dp[3] = max(dp[1] + arr[3], arr[2] + arr[3]);
+	for (int i = 4; i <= n; i++)
 	{
-		cin >> value;
-
-
+		dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
 	}
+	cout << dp[n] << "\n";
 }
